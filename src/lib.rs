5f653ef27a87657b15a91bbf2be6c99ea41b2cc3
Alex Gassner <hello@agsolutions.at>
2025-04-17 15:35:27 +0200

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@
 #[macro_use]
 extern crate napi_derive;
 
-use windows::Win32::UI::Input::KeyboardAndMouse::{keybd_event, VK_MENU, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP};
-use windows::Win32::UI::WindowsAndMessaging::{SetForegroundWindow, SetFocus};
-use windows::Win32::Foundation::{HWND};
-use windows::Win32::System::Diagnostics::Debug::GetAsyncKeyState;
+use windows::{
+  Win32::Foundation::HWND,
+  Win32::UI::Input::KeyboardAndMouse::{keybd_event, GetAsyncKeyState, SetFocus, VK_MENU, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP},
+  Win32::UI::WindowsAndMessaging::{SetForegroundWindow},
+  Win32::System::LibraryLoader::GetModuleHandleW,
+};
 
 #[napi]
 pub fn focus_window(handle_buffer: &[u8]) {
@@ -14,21 +16,21 @@ pub fn focus_window(handle_buffer: &[u8]) {
   let mut handle_bytes = [0u8; std::mem::size_of::<isize>()];
   handle_bytes.copy_from_slice(&handle_buffer[..handle_bytes.len()]);
   let handle = isize::from_ne_bytes(handle_bytes);
-  let hwnd = HWND(handle as isize);
+  let hwnd = HWND(handle  as *mut _);
 
   // Imitate pressing the Alt key if not already pressed
   let mut pressed = false;
   unsafe {
     if GetAsyncKeyState(VK_MENU.0 as i32) & 0x8000 == 0 {
       pressed = true;
-      keybd_event(VK_MENU, 0, KEYEVENTF_EXTENDEDKEY, 0);
+      keybd_event(VK_MENU.0 as u8, 0, KEYEVENTF_EXTENDEDKEY, 0);
     }
 
     SetForegroundWindow(hwnd);
     SetFocus(hwnd);
 
     if pressed {
-      keybd_event(VK_MENU, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+      keybd_event(VK_MENU.0 as u8, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
     }
   }
 }
