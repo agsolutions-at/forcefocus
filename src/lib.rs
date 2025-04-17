e89135d9ea15ac68a1177c12accb2ace0003c12b
Alex Gassner <hello@agsolutions.at>
2025-04-17 15:58:11 +0200

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,31 @@
 #![deny(clippy::all)]
 
-#[macro_use]
-extern crate napi_derive;
+use napi::bindgen_prelude::{Buffer, Error, Result};
+use napi_derive::napi;
 
 use windows::{
   Win32::Foundation::HWND,
-  Win32::UI::Input::KeyboardAndMouse::{keybd_event, GetAsyncKeyState, SetFocus, VK_MENU, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP},
-  Win32::UI::WindowsAndMessaging::{SetForegroundWindow},
+  Win32::UI::Input::KeyboardAndMouse::{
+    keybd_event, GetAsyncKeyState, SetFocus, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP, VK_MENU,
+  },
+  Win32::UI::WindowsAndMessaging::SetForegroundWindow,
 };
 
 #[napi]
-pub fn focus_window(handle_buffer: &[u8]) {
-  // Convert buffer to a LONG_PTR, then to HWND
-  let mut handle_bytes = [0u8; std::mem::size_of::<isize>()];
-  handle_bytes.copy_from_slice(&handle_buffer[..handle_bytes.len()]);
-  let handle = isize::from_ne_bytes(handle_bytes);
-  let hwnd = HWND(handle  as *mut _);
+pub fn focus_window(handle_buffer: Buffer) -> Result<()> {
+  // Validate size
+  if handle_buffer.len() != size_of::<isize>() {
+    return Err(Error::from_reason("Invalid HWND buffer size"));
+  }
+
+  // Convert &[u8] to isize
+  let handle = isize::from_ne_bytes(
+    handle_buffer[..]
+      .try_into()
+      .expect("slice with incorrect length"),
+  );
+
+  let hwnd = HWND(handle as *mut _);
 
   // Imitate pressing the Alt key if not already pressed
   let mut pressed = false;
@@ -29,8 +39,14 @@ pub fn focus_window(handle_buffer: &[u8]) {
     SetFocus(Some(hwnd));
 
     if pressed {
-      keybd_event(VK_MENU.0 as u8, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+      keybd_event(
+        VK_MENU.0 as u8,
+        0,
+        KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+        0,
+      );
     }
   }
-}
 
+  Ok(())
+}
